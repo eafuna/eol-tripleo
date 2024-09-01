abd64c51d5ebb636d2f6a836ba1b70f832c1d9eb
natut <infinity.enoch@gmail.com>
2024-09-01 19:42:33 +0800

    debugging...

--- initial.sh
+++ initial.sh
@@ -6,6 +6,9 @@
 user=`whoami`
 
 if [ "$user" == "root" ]
+    # REQUIRES:
+    #       - curl
+
     # Create 'stack' user and add it to sudoers
     useradd stack && (echo "undercloud"; echo "undercloud") | passwd stacksh
     echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/stack
@@ -17,6 +20,8 @@ if [ "$user" == "stack" ]
     # CentOS, disable subscription as this is not needed
     sed -i -e 's/enabled\=1/enable\=0/g' /etc/yum/pluginconf.d/subscription-manager.conf
     cat /etc/yum/pluginconf.d/subscription-manager.conf
+
+    yum install git -y
 
     export VIRTHOST=127.0.0.2
 
