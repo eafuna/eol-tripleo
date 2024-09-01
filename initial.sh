ec0fd211d19232c1adce00889023395e6673457b
natut <infinity.enoch@gmail.com>
2024-09-01 19:51:38 +0800

    updating..

--- initial.sh
+++ initial.sh
@@ -5,7 +5,7 @@
 
 user=`whoami`
 
-if [ "$user" == "root" ]
+if [ "$user" == "root" ]; then
     # REQUIRES:
     #       - curl
 
@@ -16,15 +16,15 @@ if [ "$user" == "root" ]
     sudo -u stack initial.sh
 fi 
 
-if [ "$user" == "stack" ]
+if [ "$user" == "stack" ]; then
     # CentOS, disable subscription as this is not needed
     sed -i -e 's/enabled\=1/enable\=0/g' /etc/yum/pluginconf.d/subscription-manager.conf
     cat /etc/yum/pluginconf.d/subscription-manager.conf
 
     yum install git -y
 
-    export VIRTHOST=127.0.0.2
+    export VIRTHOST="127.0.0.2"
 
     # https://github.com/openstack-archive/tripleo-quickstart.git
-    git clone https://github.com/eafuna/tripleo-quickstart-eol-update.git tripleo-quickstart
+    git clone "https://github.com/eafuna/eol-tripleo.git" tripleo-quickstart
 if
