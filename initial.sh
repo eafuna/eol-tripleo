4d15eaeb165333c926ab0b87720b0e21d4679a84
natut <infinity.enoch@gmail.com>
2024-09-01 19:41:10 +0800

    run script depending on sudoer

--- initial.sh
+++ initial.sh
@@ -3,18 +3,23 @@
 # sudo -i JUST TO MAKE SURE WE HAVE ALL PRIV. DO NOT SKIP!
 # curl -O https://raw.githubusercontent.com/eafuna/eol-tripleo/main/initial.sh
 
-# Create 'stack' user and add it to sudoers
-useradd stack
-(echo "undercloud"; echo "undercloud") | passwd stack
-echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/stack
-chmod 0440 /etc/sudoers.d/stack
-su – stack
+user=`whoami`
 
-# CentOS, disable subscription as this is not needed
-sed -i -e 's/enabled\=1/enable\=0/g' /etc/yum/pluginconf.d/subscription-manager.conf
-cat /etc/yum/pluginconf.d/subscription-manager.conf
+if [ "$user" == "root" ]
+    # Create 'stack' user and add it to sudoers
+    useradd stack && (echo "undercloud"; echo "undercloud") | passwd stacksh
+    echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/stack
+    chmod 0440 /etc/sudoers.d/stack
+    sudo -u stack initial.sh
+fi 
 
-export VIRTHOST=127.0.0.2
+if [ "$user" == "stack" ]
+    # CentOS, disable subscription as this is not needed
+    sed -i -e 's/enabled\=1/enable\=0/g' /etc/yum/pluginconf.d/subscription-manager.conf
+    cat /etc/yum/pluginconf.d/subscription-manager.conf
 
-# https://github.com/openstack-archive/tripleo-quickstart.git
-git clone https://github.com/eafuna/tripleo-quickstart-eol-update.git tripleo-quickstart
+    export VIRTHOST=127.0.0.2
+
+    # https://github.com/openstack-archive/tripleo-quickstart.git
+    git clone https://github.com/eafuna/tripleo-quickstart-eol-update.git tripleo-quickstart
+if
