18c79417fb57ae15de892e1cffd1162f91f42b60
natut <infinity.enoch@gmail.com>
2024-09-02 14:08:57 +0800

    debug..

--- initial0.sh
+++ initial0.sh
@@ -13,12 +13,12 @@ if [ "$user" == "root" ]; then
     if [ "$os"=="CentOS" ]; then   
         echo "CentOS detected; updating subscription disabled" 
         sed -i -e 's/enabled\=1/enable\=0/g' /etc/yum/pluginconf.d/subscription-manager.conf
-        cat /etc/yum/pluginconf.d/subscription-manager.conf
+        # cat /etc/yum/pluginconf.d/subscription-manager.conf
     fi 
 
     getent passwd stack > /dev/null 2&>1
     if [ $? -eq 0 ]; then
-        echo "Stack user exists. Skipping creation"
+        echo "..... Stack user exists. Skipping creation"
     else
         echo "Stack user not exist. Creating"
         useradd stack && (echo "undercloud"; echo "undercloud") | passwd stack
@@ -34,9 +34,10 @@ if [ "$user" == "root" ]; then
     fi    
 
     #curl -O https://raw.githubusercontent.com/eafuna/eol-tripleo/main/initial.sh /home/stack/  
-    echo "invoking stack to run initial script"
-    pwd 
+    echo "Invoking stack to run initial script"
     sudo -u stack /home/stack/tripleo-quickstart/initial0.sh
+    echo "...[DEBUG] Should reach here quickly"
+    pwd 
     # curl -O https://raw.githubusercontent.com/eafuna/eol-tripleo/main/initial.sh 
     # chmod +x initial.sh
 fi 
@@ -46,6 +47,8 @@ if [ "$user" == "stack" ]; then
 
     export VIRTHOST="127.0.0.2"
 
+    pwd
+    cd tripleo-quickstart/
     bash quickstart.sh --install-deps
 
     # https://github.com/openstack-archive/tripleo-quickstart.git
